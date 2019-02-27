44fbad461555ba842bc66a75927fc803ee71bd94
Maksim Antonenko <Maksim_Antonenko@epam.com>
2019-02-27 10:41:12 +0400

    task complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let check = [];
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++)
+  {
+    if (check[i] == 1) continue;
+    if ((preferences[preferences[i] - 1] != i + 1)
+      && (preferences[preferences[preferences[i] - 1] - 1] == i + 1))
+      {
+        count++;
+        check[preferences[i] - 1] = 1;
+        check[preferences[preferences[i] - 1] - 1] = 1;
+      }
+  }
+  return count;
 };
